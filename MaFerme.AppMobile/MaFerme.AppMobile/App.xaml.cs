4e90d2c4fd65ba9afe84c739f251edaa26d96ffe
Nacoumblé Olivier Martial SORO <olivier.soro@outlook.com>
2020-09-26 18:00:36 -0400

    Initialisation AppMobile

---
+++ MaFerme.AppMobile/MaFerme.AppMobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MaFerme.AppMobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaFerme.AppMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Login()
+            {
+                //BackgroundColor = Color.White
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
